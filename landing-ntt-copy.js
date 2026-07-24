3379a87e516570d2a17e5645fe1419d8451b3cd4
Minh Nguyen <binhminh060105140205@gmail.com>
2026-07-24 15:40:06 +0700

    feat: tailor landing copy for NttClass

---
+++ landing-ntt-copy.js
@@ -0,0 +1,1 @@
+['nav','hero','about','gallery','contact'].forEach(function(name){var script=document.createElement('script');script.src='landing-ntt-copy-'+name+'.js';document.head.appendChild(script)});
